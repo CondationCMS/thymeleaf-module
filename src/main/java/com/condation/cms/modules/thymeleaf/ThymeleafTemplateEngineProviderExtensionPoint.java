ea9b5d4019960bf11aa2d8d198d09e2d9539d898
Thorsten Marx <t.marx@eggheads.de>
2024-12-10 14:25:34 +0100

    remove deprecations

--- src/main/java/com/condation/cms/modules/thymeleaf/ThymeleafTemplateEngineProviderExtensionPoint.java
+++ src/main/java/com/condation/cms/modules/thymeleaf/ThymeleafTemplateEngineProviderExtensionPoint.java
@@ -22,7 +22,7 @@ package com.condation.cms.modules.thymeleaf;
  * #L%
  */
 
-import com.condation.cms.api.extensions.TemplateEngineProviderExtentionPoint;
+import com.condation.cms.api.extensions.TemplateEngineProviderExtensionPoint;
 import com.condation.cms.api.template.TemplateEngine;
 import com.condation.modules.api.annotation.Extension;
 
@@ -30,8 +30,8 @@ import com.condation.modules.api.annotation.Extension;
  *
  * @author t.marx
  */
-@Extension(TemplateEngineProviderExtentionPoint.class)
-public class ThymeleafTemplateEngineProviderExtentionPoint extends TemplateEngineProviderExtentionPoint {
+@Extension(TemplateEngineProviderExtensionPoint.class)
+public class ThymeleafTemplateEngineProviderExtensionPoint extends TemplateEngineProviderExtensionPoint {
 
 	
 	@Override
